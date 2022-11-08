9f072db4da207bc94ef0ed83092d7082c2477142
shakaobi <camesha.goods1@gmail.com>
2022-11-08 09:47:52 -0500

    lol

---
+++ cookieClicker/index.js
@@ -0,0 +1,6 @@
+// Your website should have two main components: an image of a cookie and a counter that starts at 0. Whenever the cookie is clicked, the counter should go up by one.
+
+const button = document.querySelector("input");
+button.addEventListener("click", function () {
+  console.log("Hello world!");
+});
